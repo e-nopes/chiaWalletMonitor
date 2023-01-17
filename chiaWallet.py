2e790368533ac015b114700373da2234e258604a
Your Name <you@example.com>
2023-01-17 18:55:22 +0000

    discard unused hooks, update explorer to xchscan, 3 min poll

--- chiaWallet.py
+++ chiaWallet.py
@@ -1,18 +1,17 @@
-import datetime #for reading present date
+import datetime
 import time 
 import os
-import requests #for retreiving coronavirus data from web
-from plyer import notification #for getting notification on your PC
+import requests
 
 enableSysTray = False
 if os.name == 'nt':
     from infi.systray import SysTrayIcon
     enableSysTray = True
 
-walletaddress = 'xch1XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+walletaddress = 'xch1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx''
 
-sendDiscord = False
-discordWebhook = r'https://discord.com/api/webhooks/0000000000000000/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+sendDiscord = True
+discordWebhook = r'https://discord.com/api/webhooks/0000000000000000/xxxxxxxxxxxxxxxxxxxxxxxxxxxx''
 
 #Send push notification over Pushover?
 sendPushover = False
@@ -52,8 +51,9 @@ if enableSysTray:
 
 #BEGIN
 
+
 #let there is no data initially
-chiWallet = None
+chiaWallet = None
 currXCH = 0
 netBalance = -1
 firstRun = True
@@ -66,15 +66,16 @@ headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWe
 
 while(True):
 
+    shouldPrint = None
     try:
         
-        chiaWallet = requests.get("https://api2.chiaexplorer.com/address/" + walletaddress, headers=headers)
+        chiaWallet = requests.get("https://xchscan.com/api/account/balance?address=" + walletaddress, headers=headers)
     except:
         print("Please! Check your internet connection")
 
     if (chiaWallet != None):
         data = chiaWallet.json()
-        netBalance = data['netBalance']/1000000000000
+        netBalance = data['xch']
 
         if (firstRun == True):
             shouldPrint = True
@@ -84,7 +85,7 @@ while(True):
         else:
             if currXCH != netBalance:
                 shouldPrint = True
-                msgTxt = "Your Chia balance has changed, for a total of {netBalance} XCH, Chia Pet!".format(netBalance = data['netBalance']/1000000000000)
+                msgTxt = "Your Chia balance has changed, for a total of {netBalance} XCH!".format(netBalance = data['xch'])
                 msgTitle = 'Congrats, Chia Farmer!'
                 
                 
@@ -94,34 +95,10 @@ while(True):
                     notifier = dn.Notifier(discordWebhook)
                     notifier.send(discord_info, print_message=False)
                 
-                if sendPushover == True:
-                    from pushover import init, Client
-                    client = Client(pushoverUserKey, api_token=pushoverAPIKey)
-                    client.send_message(msgTxt, title=msgTitle) #pip install python-pushover
-                        
-                if playSound == True:
-                    from playsound import playsound #pip install playsound
-                    playsound(song)
-                
-                if sendSlack == True:
-                    post_message_to_slack(msgTxt)
-                    
-                if sendPushBullet == True:
-                    from pushbullet import Pushbullet
-                    pb = Pushbullet(pbAPIKey)
-                    push = pb.push_note(msgTitle, msgTxt)
-
     if shouldPrint == True:
         print(msgTxt)
-        notification.notify(
-            title = msgTitle,
-            message = msgTxt ,
-            app_icon = os.path.join(os.path.dirname(os.path.realpath(__file__)), "chia.ico"),
-            app_name = "Chia Wallet Monitor",
-            timeout  = 50
-        )
     
     shouldPrint = False
     firstRun = False
     currXCH = netBalance
-    time.sleep(10*3)
+    time.sleep(60*3)
