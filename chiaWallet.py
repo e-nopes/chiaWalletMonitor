c2f9b1bfd737c659319f338774d342168137416a
Your Name <you@example.com>
2023-01-23 04:57:44 +0000

    multiple wallet support

--- chiaWallet.py
+++ chiaWallet.py
@@ -1,64 +1,26 @@
-import datetime
+import datetime #for reading present date
 import time 
 import os
-import requests
+import requests #for retreiving coronavirus data from web
 
 enableSysTray = False
 if os.name == 'nt':
     from infi.systray import SysTrayIcon
     enableSysTray = True
 
-walletaddress = 'xch1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx''
+walletaddress = 'xch1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+addressList   = {   'wallet1'  :    'xch1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' ,
+                    'wallet2'  :    'xch1xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' }
 
 sendDiscord = True
-discordWebhook = r'https://discord.com/api/webhooks/0000000000000000/xxxxxxxxxxxxxxxxxxxxxxxxxxxx''
-
-#Send push notification over Pushover?
-sendPushover = False
-pushoverUserKey = 'xxxxxxxxxx'
-pushoverAPIKey = 'xxxxxxxxxxxxxxxx'
-
-#Play a custom sound file?
-playSound = False
-song = 'audio.mp3'
-
-#Send a slack notification?
-sendSlack = False
-slack_token = 'xoxb-my-bot-token'
-slack_channel = '#my-channel'
-slack_icon_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuGqps7ZafuzUsViFGIremEL2a3NR0KO0s0RTCMXmzmREJd5m4MA&s'
-slack_user_name = 'Chia Wallet Monitor'
+discordWebhook = r'https://discord.com/api/webhooks/0000000000000000/xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 
-#SendPushBullet notification?
-sendPushBullet = False
-pbAPIKey = 'XXXXXXXXXXXXXXXXXXXXX'
-
-def on_quit_callback(systray):
-    print("QUIT!")
-    if systray:
-        systray.shutdown()
-   
-
-def checkWallet(systray):
-    print("Wallet check")
-
-menu_options = (("Check Wallet", None, checkWallet),)
-if enableSysTray:
-    systray = SysTrayIcon("chia.ico", "ChiaWalletMonitor", menu_options, on_quit=on_quit_callback)
-
-
-
-#BEGIN
-
-
-#let there is no data initially
 chiaWallet = None
-currXCH = 0
-netBalance = -1
-firstRun = True
-if enableSysTray:
-    systray.start()
+currXCH = dict.fromkeys(addressList.keys(),0)       #rememberd balances
+netBalance = dict.fromkeys(addressList.keys(),0)    #taken from chiaWalletRequest
+firstRun = dict.fromkeys(addressList.keys(),True)   #init
 
 headers = requests.utils.default_headers()
 print(headers)      
@@ -66,39 +28,43 @@ headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWe
 
 while(True):
 
-    shouldPrint = None
+    shouldPrint = dict.fromkeys(addressList.keys(),False)
+
     try:
-        
-        chiaWallet = requests.get("https://xchscan.com/api/account/balance?address=" + walletaddress, headers=headers)
-    except:
-        print("Please! Check your internet connection")
+        # https://xchscan.com/rest-api 
+        for name in addressList.keys():
+            #print("Requesting ", name, addressList[name])
+            chiaWallet = requests.get("https://xchscan.com/api/account/balance?address=" + addressList[name], headers=headers)
 
-    if (chiaWallet != None):
-        data = chiaWallet.json()
-        netBalance = data['xch']
+            if (chiaWallet != None):
+                data = chiaWallet.json()
+                netBalance[name] = data['xch']
 
-        if (firstRun == True):
-            shouldPrint = True
-            msgTitle = "Your wallet as of {}".format(datetime.date.today())
-            msgTxt = "You have a total of {} XCH, Farmer!".format(netBalance)
+                if (firstRun[name] == True):
+                    shouldPrint[name] = True
+                    msgTxt = '* {wallet} has a total of {} XCH'.format(float(netBalance[name]), wallet = name )
+                    
+                else:
+                    if currXCH[name] != netBalance[name]:
+                        shouldPrint[name] = True
+                        amount = netBalance[name] - currXCH[name]
+                        msgTxt = "{wallet} balance changed {sign}{amount:0.5}, {netBalance:0.5} XCH total".format(wallet = name, sign= '+' if (amount>0) else '', amount = amount, netBalance = netBalance[name])
+                        
+                        import discord_notify as dn
+                        discord_info = msgTxt
+                        notifier = dn.Notifier(discordWebhook)
+                        notifier.send(discord_info, print_message=False)
+
+                currXCH[name] = netBalance[name]
             
-        else:
-            if currXCH != netBalance:
-                shouldPrint = True
-                msgTxt = "Your Chia balance has changed, for a total of {netBalance} XCH!".format(netBalance = data['xch'])
-                msgTitle = 'Congrats, Chia Farmer!'
-                
-                
-                if sendDiscord == True:
-                    import discord_notify as dn
-                    discord_info = msgTxt
-                    notifier = dn.Notifier(discordWebhook)
-                    notifier.send(discord_info, print_message=False)
-                
-    if shouldPrint == True:
-        print(msgTxt)
-    
-    shouldPrint = False
-    firstRun = False
-    currXCH = netBalance
+            if shouldPrint[name] == True:
+                print(msgTxt)
+            
+            shouldPrint[name] = False
+            firstRun[name] = False
+            time.sleep(3)
+           
+    except Exception as e: 
+        print (e)
+
     time.sleep(60*3)
